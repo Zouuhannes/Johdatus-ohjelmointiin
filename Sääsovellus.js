e8af03fcb82db52a42ff85113d9a1e31501fadea
Zouuhannes <151924544+Zouuhannes@users.noreply.github.com>
2023-12-07 15:07:18 +0200

    UUs vERSO
    Uus veriso

--- Sääsovellus.js
+++ Sääsovellus.js
@@ -1,83 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Sääsovellus</title>
-    <link rel="stylesheet" href="Sääsovellus.css" />
-  </head>
-  <body>
-    <script src="Sääsovellus.js"></script>
-    <div class="card">
-      <div class="search">
-        <input type="text" placeholder="Valitse Kaupunki" spellcheck="false" />
-        <button><img src="Kuvat/Hae.jpg" /></button>
-      </div>
-      <div class="weather">
-        <img src="Kuvat/rain.jpg" class="weather-icon" />
-        <h1 class="temp">22°c</h1>
-        <h2 class="kaupunki">New York</h2>
-        <div class="details">
-          <div class="col">
-            <img src="Kuvat/humidity.jpg" />
-            <div>
-              <p class="humidity">50%</p>
-              <p>Humidity</p>
-            </div>
-          </div>
-          <div class="col">
-            <img src="kuvat/wind2.jpg" />
-            <div>
-              <p class="wind">15 km/h</p>
-              <p>Wind Speed</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <script>
-      const apiKey = "f38f7eb98d775a523a6213cfd516a561";
-      const apiUrl =
-        "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+   function getCurrentLocation() {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(
+            (position) => {
+              const latitude = position.coords.latitude;
+              const longitude = position.coords.longitude;
 
-      const searchBox = document.querySelector(".search input");
-      const searchBtn = document.querySelector(".search button");
-      const weatherIcon = document.querySelector(".weather-icon");
+              // Use the coordinates to fetch the nearest city
+              const reverseGeocodeUrl = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`;
 
-      async function checkWeather(city) {
-        const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-        var data = await response.json();
+              fetch(reverseGeocodeUrl)
+                .then((response) => response.json())
+                .then((locationData) => {
+                  // Extract the city name from the reverse geocoding response
+                  const cityName =
+                    locationData.address.city 
+                    locationData.address.town 
+                    locationData.address.village ||
+                    locationData.address.suburb;
 
-        console.log(data);
-
-        document.querySelector(".kaupunki").innerHTML = data.name;
-        document.querySelector(".temp").innerHTML =
-          Math.round(data.main.temp) + "°c";
-        document.querySelector(".humidity").innerHTML =
-          data.main.humidity + "%";
-        document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
-
-        if (data.weather[0].main == "Clouds") {
-          weatherIcon.src = "Kuvat/cloudy.jpg";
-        } else if (data.weather[0].main == "Clear") {
-          weatherIcon.src = "Kuvat/clear.png";
-        } else if (data.weather[0].main == "Rain") {
-          weatherIcon.src = "Kuvat/rain.jpg";
-        } else if (data.weather[0].main == "Drizzle") {
-          weatherIcon.src = "Kuvat/drizzle.jpg";
-        } else if (data.weather[0].main == "Mist") {
-          weatherIcon.src = "Kuvat/mist.jpg";
-        } else if (data.weather[0].main == "Snow") {
-          weatherIcon.src = "Kuvat/Snow.png";
-        } else if (data.weather[0].main == "Rain") {
-          weatherIcon.src = "Kuvat/rain.jpg";
+                  // Use the city name to fetch weather data
+                  checkWeather(cityName);
+                })
+                .catch((error) => {
+                  console.error(
+                    "Error fetching reverse geocoding data:",
+                    error
+                  );
+                });
+            },
+            (error) => {
+              console.error("Error getting location:", error);
+            }
+          );
+        } else {
+          alert("Geolocation is not supported by your browser.");
         }
-
-        document.querySelector(".weather").style.display = "block";
       }
-      searchBtn.addEventListener("click", () => {
-        checkWeather(searchBox.value);
-      });
-    </script>
-  </body>
-</html>
+    
+sää();

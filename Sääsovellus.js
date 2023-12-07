3044112e1bc599825093164ead7d81696dcf94b7
Zouuhannes <151924544+Zouuhannes@users.noreply.github.com>
2023-12-07 17:28:14 +0200

    Final
    valmis
    
    Co-Authored-By: NappiPastori <151924891+NappiPastori@users.noreply.github.com>

--- Sääsovellus.js
+++ Sääsovellus.js
@@ -1,24 +1,26 @@
-   function getCurrentLocation() {
+  function getCurrentLocation() {
         if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(
             (position) => {
               const latitude = position.coords.latitude;
               const longitude = position.coords.longitude;
 
-              // Use the coordinates to fetch the nearest city
+              // Haetaan koordinaattien avulla käyttäjän lähin kaupunki
+
               const reverseGeocodeUrl = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`;
 
               fetch(reverseGeocodeUrl)
                 .then((response) => response.json())
                 .then((locationData) => {
-                  // Extract the city name from the reverse geocoding response
+        
                   const cityName =
                     locationData.address.city 
                     locationData.address.town 
                     locationData.address.village ||
                     locationData.address.suburb;
 
-                  // Use the city name to fetch weather data
+                  // Tarkistaa kaupungin sään nimen mukaan
+
                   checkWeather(cityName);
                 })
                 .catch((error) => {
